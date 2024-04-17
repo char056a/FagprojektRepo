07b9236ea364be2c0a3550128856a337c2684070
LFrankensteiner <louise.blankensteiner@gmail.com>
2024-04-17 09:31:00 +0200

    oopsi doopsi

--- extendedmodel.py
+++ extendedmodel.py
@@ -3,35 +3,52 @@ import json
 import matplotlib.pyplot as plt
 from scipy.integrate import simpson
 
-class MVPmodel:
-    def __init__(self,**kwargs):
-        with open('MVPconfig.json', 'r') as f:
+class patient:
+    def __init__(self, model = "EHM", patient_type = 0,**kwargs):
+        self.model = model.upper()
+        with open('config.json', 'r') as f:
             defaults = json.load(f)
-        
+
+        with open(f'{self.model}config.json', 'r') as f:
+            defaults.update(json.load(f))
+
         defaults.update(kwargs)
-        
+
         for key, value in defaults.items():
             setattr(self, key, value)
 
-        self.x0 = [self.D1, self.D2, self.Isc, self.Ip, self.Ieff, self.G, self.Gsc]
-        self.x = [self.D1, self.D2, self.Isc, self.Ip, self.Ieff, self.G, self.Gsc]
+        for key in self.state_keys:
+            setattr(self, key+"0", defaults[key])
+        # 
+        if self.model == "EHM":
+            self.f_func = lambda *args : EHM(self, *args)
+        if self.model == "MVP":
+            self.f_func = lambda *args : MVP(self, *args)
+            
 
     def __str__(self):
         return str(self.__dict__)
         
+    def get_state(self):
+        """Update state vector to values given by input"""
+        x = [getattr(self,key) for key in self.state_keys]
+        return x
+
+
     def update_state(self, x_new):
         """Update state vector to values given by input"""
-        self.x = x_new
-        self.D1, self.D2, self.Isc, self.Ip, self.Ieff, self.G, self.Gsc = x_new
+        for key, val in zip(self.state_keys, x_new):
+            setattr(self, key, val)
         return
 
     def reset(self):
         """Resets state to x0"""
-        self.update_state(self.x0)
+        x0 = [getattr(self,key+"0") for key in self.state_keys]
+        self.update_state(x0)
         return
 
     def time_arr(self, length):
-        return np.arange(0, length*self.tausc, self.tausc)
+        return np.arange(0, length*self.timestep, self.timestep)
 
     def euler_step(self, dx):
         """
@@ -42,36 +59,11 @@ class MVPmodel:
         dx : numpy array
             Derivative of state vector.
         """
-        x_new = self.x + dx * self.tausc
+        x_new = self.get_state() + dx * self.timestep
         self.update_state(x_new)
         return
 
-    def f(self, u, d):
-        """
-        Solves dx = f(x, u, d)
 
-        Parameters
-        ----------
-        u : int or float 
-            Insulin injection rate.
-        d : int or float 
-            Meal ingestion rate.
-        
-        Returns
-        -------
-        dx : numpy array
-            Solution to system of differential equations. 
-        """
-        dD1 = d - self.D1/self.taum
-        dD2 = (self.D1 - self.D2)/self.taum
-        dIsc = u/(self.tau1 * self.C1) - self.Isc/self.tau1
-        dIp = (self.Isc - self.Ip)/self.tau2
-        dIeff = -self.p2 * self.Ieff + self.p2 * self.S1 * self.Ip
-        dG = - (self.gezi + self.Ieff) * self.G + self.egp0 + 1000 * self.D2 / (self.Vg * self.taum)
-        dGsc = (self.G - self.Gsc) / self.tausc
-
-        dx = np.array([dD1, dD2, dIsc, dIp, dIeff, dG, dGsc])
-        return dx
 
     def PID_controller(self, I, y, y_prev):
         """
@@ -88,13 +80,14 @@ class MVPmodel:
         """
         ek = y - self.Gbar
         Pk = self.Kp * ek
-        Ki = self.Kp * self.tausc / self.Ti
+        Ki = self.Kp * self.timestep / self.Ti
         Ikp1 = I + Ki * ek
-        Kd = self.Kp * self.Td / self.tausc
+        Kd = self.Kp * self.Td / self.timestep
         Dk = Kd * (y - y_prev)
         uk = self.us + Pk + I + Dk
         uk = max(uk, 0)
         return uk, Ikp1
+
 
     def glucose_penalty(self, G = "Default"):
         """
@@ -114,6 +107,7 @@ class MVPmodel:
         
         Returns
         -------
+
         p(G) : float
             Penalty
         """
@@ -122,13 +116,12 @@ class MVPmodel:
         return 1/2 * (G - self.Gbar)**2 + self.kappa/2 * max((self.Gmin - G), 0)**2
  
 
+
     def bolus_sim(self, bolus, meal_size, meal_idx = 0, iterations = 100, plot = False):
         ds = np.zeros(iterations)
         us = np.ones(iterations) * self.us
-
-        ds[meal_idx] = meal_size / self.tausc # Ingestion 
-        us[0] += bolus * 1000 / self.tausc
-
+        ds[meal_idx] = meal_size / self.timestep # Ingestion 
+        us[0] += bolus * 1000 / self.timestep
         states, _ = self.simulate(ds, us)
         Gt = states[:, 5]
         p = np.array([self.glucose_penalty(G = G) for G in Gt])
@@ -138,21 +131,20 @@ class MVPmodel:
             fig, ax = plt.subplots(1,2)
             ax[0].plot(t, p)
             ax[1].plot(t, Gt)
-
             ax[0].set_xlabel("time(h)")
             ax[1].set_xlabel("time(h)")
             ax[1].set_ylabel("g")
-
 
             ax[0].set_title("Penalty Function")
             ax[1].set_title("Blood Glucose")
             plt.show()
         return phi, p, Gt
 
+
     def simulate(self, ds, u_func=None):
         """
         Simulates patient.
-        
+
         Parameters
         ----------
         ds : numpy array
@@ -172,7 +164,6 @@ class MVPmodel:
         res = [self.x]
         inp = 0
         u = self.us
-
         if isinstance(u_func, (np.ndarray, list)):
             u = u_func[0]
             inp = 1
@@ -191,6 +182,7 @@ class MVPmodel:
             u = u_func
             def get_u(inp):
                 return u_func, inp
+
         else:
             def get_u(inp):
                 return self.us, inp
@@ -200,13 +192,14 @@ class MVPmodel:
         for d in ds[:-1]:
             dx = self.f(u, d)
             self.euler_step(dx)     
-            res.append(self.x)
+            res.append(self.get_state())
             u, inp = get_u(inp)
             u_list.append(u)
         dx = self.f(u, d)
         self.euler_step(dx)     
         res.append(self.x)
         return np.array(res), u_list
+
 
 
     def plot(self, data, u, d):
@@ -221,22 +214,18 @@ class MVPmodel:
 
         ax[0,1].set_title("Insulin Injection Rate")
         ax[0,1].plot(t[:-1], u[:len(t) - 1]) # Lidt bøvet måde at håndtere det her på, men det går nok.
-
         ax[1,0].set_title("Carbs")
         ax[1,0].plot(t, data[:,0], label= "D1")
         ax[1,0].plot(t, data[:,1], label= "D2")
         ax[1,0].legend()
         ax[1,1].set_title("Carb Ingestion (d)")
         ax[1,1].plot(t[:-1], d)
-
         ax[2,0].set_title("Insulin")
         ax[2,0].plot(t, data[:,2], label= "Subcutaneous")
         ax[2,0].plot(t, data[:,3], label= "Plasma")
         ax[2,0].legend()
-
         ax[2,1].plot(t, data[:,4])
         ax[2,1].set_title("Effective Insulin")
-
 
         ax[0,0].set_ylabel("mg/dL")
         ax[0,1].set_ylabel("mU/min")
@@ -250,18 +239,16 @@ class MVPmodel:
         fig.tight_layout()
         return
 
+
     def optimal_bolus(self, meal_idx = 0, min_U = 0, max_U = 75, min_meal = 30, max_meal = 150, n = 50):
         Us = np.linspace(min_U, max_U, n)
         meals = np.linspace(min_meal, max_meal, n)
-
         res = np.empty((len(meals), len(Us), 3))
-
         for i, d0 in enumerate(meals):
             for j, U in enumerate(Us):
                 self.reset()
                 phi, _, _ = self.bolus_sim(U, d0, meal_idx=meal_idx)
                 res[n - 1 - j ,i] = [phi]*3
-
         best = np.argmin(res[:,:,0], axis=0)
 
         r = 1 / (res.max() - res.min())
@@ -274,6 +261,83 @@ class MVPmodel:
         plt.ylabel("Bolus Size (U)")
         plt.xlabel("Meal Size (g. CHO)")
 
-
         best_us = [Us[n - 1- i] for i in best]
         return meals, best_us
+
+
+
+def MVP(self, u, d):
+    """
+    Solves dx = f(x, u, d)
+
+    Parameters
+    ----------
+    u : int or float 
+        Insulin injection rate.
+    d : int or float 
+        Meal ingestion rate.
+    
+    Returns
+    -------
+    dx : numpy array
+        Solution to system of differential equations. 
+    """
+    dD1 = d - self.D1/self.taum
+    dD2 = (self.D1 - self.D2)/self.taum
+    dIsc = u/(self.tau1 * self.C1) - self.Isc/self.tau1
+    dIp = (self.Isc - self.Ip)/self.tau2
+    dIeff = -self.p2 * self.Ieff + self.p2 * self.S1 * self.Ip
+    dG = - (self.gezi + self.Ieff) * self.G + self.egp0 + 1000 * self.D2 / (self.Vg * self.taum)
+    dGsc = (self.G - self.Gsc) / self.timestep
+
+    dx = np.array([dD1, dD2, dIsc, dIp, dIeff, dG, dGsc])
+    return dx
+
+
+def EHM(self, uI, uG, d):
+    """
+    Solves dx = f(x, u, d)
+
+    Parameters
+    ----------
+    u : int or float 
+        Insulin injection rate.
+    d : int or float 
+        Meal ingestion rate.
+    
+    Returns
+    -------
+    dx : numpy array
+        Solution to system of differential equations. 
+    """
+    G = self.Q1/self.VG
+    RA = self.f*self.D2/self.TauD
+    D = 1000 * d/self.MwG
+    HRmax = 220 - self.age
+    HR = self.HRR * (HRmax - self.HR0) + self.HR0
+    fE1x = self.E1/(self.a * self.HR0)**self.n
+    fE1 = fE1x/(1+fE1x)
+    F01c = min(self.F01, self.F01 * G / 4.5)
+    FR = max(0.003 * (G - 9) * self.VG, 0) 
+
+    dx = []
+    dx.append((G - self.GI)/self.TauIG)
+    dx.append(-F01c - FR - self.x1 * self.Q1 + self.k12 * self.Q2 + RA + self.EGP0 * (1 - self.x3) \
+        + self.Kglu * self.VG * self.Z2 - self.alpha * self.E2**2 * self.x1 * self.Q1)
+    dx.append(self.x1 * self.Q1 - (self.k12 + self.x2) * self.Q2 + self.alpha * self.E2**2 * self.x1 * self.Q1 \
+        - self.alpha * self.E2**2 * self.x2 * self.Q2 - self.beta * self.E1 / self.HR0)
+
+    dx.append(uI - self.S1 / self.TauS)
+    dx.append((self.S1 - self.S2)/self.TauS)
+    dx.append(self.S2 / (self.VI * self.TauS) - self.ke * self.I)
+    dx.append(self.kb1 * self.I - self.ka1 * self.x1)
+    dx.append(self.kb2 * self.I - self.ka2 * self.x2)
+    dx.append(self.kb3 * self.I - self.ka3 * self.x3)
+    dx.append(self.AG * D - self.D1 / self.TauD)
+    dx.append((self.D1 - self.D2)/self.TauD)
+    dx.append(uG - self.Z1 / self.Tauglu)
+    dx.append((self.Z1 - self.Z2)/self.Tauglu)
+    dx.append((HR - self.HR0 - self.E1)/self.TauHR)
+    dx.append(-(fE1/self.Tauin - 1/self.TE) * self.E2 + fE1 * self.TE /(self.c1 + self.c2))
+    dx.append((self.c1 * fE1 + self.c2 - self.TE)/self.Tauex)
+    return np.array(dx)
